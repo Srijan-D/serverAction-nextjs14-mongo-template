0b10a9765c67d3ccb40b9cb96a4114bb4f71a0ef
Srijan-D <srijandubey3105@gmail.com>
2024-06-21 23:46:44 +0530

    updating mongoose for product model and schema

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,30 @@
+"use client";
 import React from "react";
 
+type Product = {
+  id: number;
+  name: string;
+};
+
 const ProductList = () => {
-  return <div></div>;
+  const [products, setProducts] = React.useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch("http://localhost:3000/api/products");
+    const data = await response.json();
+    setProducts(data);
+  };
+
+  return (
+    <div>
+      <button onClick={getProducts}>Get Products</button>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ProductList;
---
+++ src/models/productModel.ts
@@ -0,0 +1,40 @@
+// Importing mongoose library along with Document and Model types from it
+import mongoose, { Document, Model } from "mongoose";
+
+// Defining the structure of a todo item using TypeScript interfaces
+export interface IProduct {
+  id: number;
+  name: string;
+}
+
+// Merging ITodo interface with mongoose's Document interface to create
+// a new interface that represents a todo document in MongoDB
+export interface IProductDocument extends IProduct, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Defining a mongoose schema for the todo document, specifying the types
+// and constraints
+const todoSchema = new mongoose.Schema<IProductDocument>(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // Automatically add 'createdAt' and 'updatedAt' fields to the document
+    timestamps: true,
+  }
+);
+
+// Creating a mongoose model for the todo document
+const Todo: Model<ITodoDocument> =
+  mongoose.models?.Todo || mongoose.model("Todo", todoSchema);
+
+export default Todo;
